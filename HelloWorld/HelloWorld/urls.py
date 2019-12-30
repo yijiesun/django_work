660454811a7d982d8c916a2b5ac302902f4e241f
yijie.sun <sunyijie520@126.com>
2019-12-30 15:44:19 +0800

    管理页面学习

--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.conf.urls import url 
 from django.urls import path 
 from HelloWorld.view import current_datetime
+admin.autodiscover()
 
 urlpatterns = [ 
-path('admin/', admin.site.urls),
+url(r'^admin/',admin.site.urls,name='admin'),
 url('^time/$', current_datetime),] 
---
+++ HelloWorld/books/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from books.models import Publisher, Author, Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')#列表显示内容
+    search_fields = ('first_name', 'last_name')#搜索框
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')#列表显示内容
+    list_filter = ('publication_date',)#列表用日期排序
+    date_hierarchy = 'publication_date'#列表用日期排序功能同上
+    ordering = ('-publication_date',)#出版日期逆序排序
+    fields = ('title', 'authors', 'publisher', 'publication_date')#编辑页面中可编辑栏目
+    filter_horizontal = ('authors',)#好用的多选框
+    
+admin.site.register(Publisher)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book,BookAdmin)
+# Register your models here.
